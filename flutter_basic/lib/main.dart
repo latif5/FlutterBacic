92ae9392ff47cff47fad506b3530d22d4fb32a9f
Latif <latif.abdurrahman@mail.ugm.ac.id>
2020-03-14 14:12:25 +0700

    02 widget text

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -8,10 +8,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("First Flutter App")),
-        body: Center(child: Container(child: Text("Hello You!")))
-      )
-    );
+        home: Scaffold(
+            appBar: AppBar(title: Text("First Flutter App")),
+            body: Center(
+                child: Container(
+                    color: Color.fromRGBO(20, 30, 40, 100),
+                    height: 30,
+                    width: 300,
+                    alignment: Alignment.center,
+                    child: Text("Hello You!",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w800))))));
   }
 }
